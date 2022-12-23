f072747e7c43d26455eb60b4787da56c04bcf54c
Jacob Zietek <jzietek@purdue.edu>
2022-12-22 22:23:37 -0500

    It somewhat works lol

--- gif-popup-menu.py
+++ gif-popup-menu.py
@@ -10,9 +10,12 @@ try:
     from StringIO import StringIO
 except ImportError:
     from io import StringIO
+import pyperclip
 
 APIKEY = "LIVDSRZULELA"
-LMT = 8
+LMT = 6
+gif_w = 740//2
+gif_h = 700//3
 
 
 ws = Tk()
@@ -22,47 +25,6 @@ ws.resizable(0, 0)
 
 gifs = []
 
-class gifplay:
-    """
-    Usage: mygif=gifplay(<<tkinter.label Objec>>,<<GIF path>>,<<frame_rate(in ms)>>)
-    example:
-    gif=GIF.gifplay(self.model2,'./res/neural.gif',0.1)
-    gif.play()
-    This will play gif infinitely
-
-    source: https://gist.github.com/gupta-shantanu/8781f72ff903c2cf3878
-    """
-    def __init__(self,label,gif_url,delay=0.1):
-        self.frame=[]
-        i=0
-
-        image_data = Image.open(urlopen(gif_url)).resize((gif_w, gif_h))
-
-        while 1:
-            try:
-                image=ImageTk.PhotoImage(image_data, format="gif -index "+str(i))
-                self.frame.append(image)
-                i=i+1
-            except:
-                break
-        print(i)
-        self.totalFrames=i-1
-        self.delay=delay
-        self.labelspace=label
-        self.labelspace.image=self.frame[0]
-
-    def play(self):
-        """
-        plays the gif
-        """
-        _thread.start_new_thread(self.infinite,())
-
-    def infinite(self):
-        i=0
-        while 1:
-            self.labelspace.configure(image=self.frame[i])
-            i=(i+1)%self.totalFrames
-            time.sleep(self.delay)
 
 @cache
 def call_tenor_api(search_str):
@@ -84,7 +46,32 @@ def fetch_top_gifs(search_str, h, j):
         tenor_gifs = json.loads(r.content)['results']
         tenor_gifs = [x['media'][0]['gif'] for x in tenor_gifs]
         gifs = tenor_gifs
-        print("gifs")
+        
+        print("updating")
+        gif1 = ImageTk.PhotoImage(Image.open(urlopen(gifs[0]["preview"])).resize((gif_w, gif_h)))
+        gif1Label.configure(image=gif1)
+        gif1Label.image = gif1
+
+        gif2 = ImageTk.PhotoImage(Image.open(urlopen(gifs[1]["preview"])).resize((gif_w, gif_h)))
+        gif2Label.configure(image=gif2)
+        gif2Label.image = gif2
+
+        gif3 = ImageTk.PhotoImage(Image.open(urlopen(gifs[2]["preview"])).resize((gif_w, gif_h)))
+        gif3Label.configure(image=gif3)
+        gif3Label.image = gif3
+
+        gif4 = ImageTk.PhotoImage(Image.open(urlopen(gifs[3]["preview"])).resize((gif_w, gif_h)))
+        gif4Label.configure(image=gif4)
+        gif4Label.image = gif4
+
+        gif5 = ImageTk.PhotoImage(Image.open(urlopen(gifs[4]["preview"])).resize((gif_w, gif_h)))
+        gif5Label.configure(image=gif5)
+        gif5Label.image = gif5
+
+        gif6 = ImageTk.PhotoImage(Image.open(urlopen(gifs[5]["preview"])).resize((gif_w, gif_h)))
+        gif6Label.configure(image=gif6)
+        gif6Label.image = gif6
+
 
 # Create a text variable for searchbar
 var = StringVar()
@@ -96,37 +83,35 @@ search.grid(row=0)
 #search.pack()
 search.focus()
 
-gif_w = 740//2
-gif_h = 700//3
+
 # Set up top 6 gifs
-gif1 = ImageTk.PhotoImage(Image.open(urlopen("https://media.tenor.com/846fTf5uEgcAAAAC/bfb-bfdi.gif")).resize((gif_w, gif_h)), format="gif")
+gif1 = ImageTk.PhotoImage(Image.open(urlopen("https://upload.wikimedia.org/wikipedia/commons/3/3e/Evangelion.png")).resize((gif_w, gif_h)))
 gif1Label = Label(ws, image=gif1)
 gif1Label.grid(column=0, row=1)
 
-gif2 = ImageTk.PhotoImage(Image.open(urlopen("https://media.tenor.com/846fTf5uEgcAAAAC/bfb-bfdi.gif")).resize((gif_w, gif_h)), format="gif")
+gif2 = ImageTk.PhotoImage(Image.open(urlopen("https://upload.wikimedia.org/wikipedia/commons/3/3e/Evangelion.png")).resize((gif_w, gif_h)))
 gif2Label = Label(ws, image=gif2)
 gif2Label.grid(column=1, row=1)
 
-gif3 = ImageTk.PhotoImage(Image.open(urlopen("https://media.tenor.com/846fTf5uEgcAAAAC/bfb-bfdi.gif")).resize((gif_w, gif_h)), format="gif")
+gif3 = ImageTk.PhotoImage(Image.open(urlopen("https://upload.wikimedia.org/wikipedia/commons/3/3e/Evangelion.png")).resize((gif_w, gif_h)))
 gif3Label = Label(ws, image=gif3)
 gif3Label.grid(column=0, row=2)
 
-gif4 = ImageTk.PhotoImage(Image.open(urlopen("https://media.tenor.com/846fTf5uEgcAAAAC/bfb-bfdi.gif")).resize((gif_w, gif_h)), format="gif")
+gif4 = ImageTk.PhotoImage(Image.open(urlopen("https://upload.wikimedia.org/wikipedia/commons/3/3e/Evangelion.png")).resize((gif_w, gif_h)))
 gif4Label = Label(ws, image=gif4)
 gif4Label.grid(column=1, row=2)
 
-gif5 = ImageTk.PhotoImage(Image.open(urlopen("https://media.tenor.com/846fTf5uEgcAAAAC/bfb-bfdi.gif")).resize((gif_w, gif_h)), format="gif")
+gif5 = ImageTk.PhotoImage(Image.open(urlopen("https://upload.wikimedia.org/wikipedia/commons/3/3e/Evangelion.png")).resize((gif_w, gif_h)))
 gif5Label = Label(ws, image=gif5)
 gif5Label.grid(column=0, row=3)
 
-
-gif6Label = Label(ws)
+gif6 = ImageTk.PhotoImage(Image.open(urlopen("https://upload.wikimedia.org/wikipedia/commons/3/3e/Evangelion.png")).resize((gif_w, gif_h)))
+gif6Label = Label(ws, image=gif6)
 gif6Label.grid(column=1, row=3)
 
-gif6 = gifplay(gif6Label, "https://media.tenor.com/846fTf5uEgcAAAAC/bfb-bfdi.gif")
 
 """
-gif6 = ImageTk.PhotoImage(Image.open(urlopen("https://media.tenor.com/846fTf5uEgcAAAAC/bfb-bfdi.gif")).resize((gif_w, gif_h)), format="gif")
+gif6 = ImageTk.PhotoImage(Image.open(urlopen("https://upload.wikimedia.org/wikipedia/commons/3/3e/Evangelion.png")).resize((gif_w, gif_h)), format="gif")
 gif6Label = Label(ws, image=gif6)
 gif6Label.grid(column=1, row=3)
 """
